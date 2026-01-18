91d50cfe48ead153039fb821ede89fcd57600c6a
Sherya <DjVooLF@yandex.ru>
2026-01-18 19:06:32 +0400

    Work1

--- Source/SnakeGame/SnakeBase.cpp
+++ Source/SnakeGame/SnakeBase.cpp
@@ -10,7 +10,7 @@ ASnakeBase::ASnakeBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	ElementSize = 100.f;
-	MovementSpeed = 0,1.f;
+	MovementSpeed = 0,5.f;
 	LastMoveDirection = EMovementDirection::DOWN;
 }
 
